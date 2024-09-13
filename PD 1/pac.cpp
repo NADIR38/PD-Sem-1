2e18158221b70e79c15e5f3027da9418c78b97ea
NADIR38 <jamalnadir7778@gmail.com>
2024-09-13 08:21:47 -0700

    pac

---
+++ PD 1/pac.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+main()
+{
+cout<< " ########  ********   @@@@@@@@  m m      mm    aaaaaaaa   m n      m                                       "<<endl;
+cout<< " #      #  *      %   ^         m  m    m m    *      %   m  n     m                  "<<endl;
+cout<< " #      #  *&&&&&&%   ^         m   m  m  m    *&&&&&&%   m   n    m                       "<<endl;
+cout<< " ########  *      %   ^         m    m    m    *      %   m    n   m                       "<<endl;
+cout<< " #         *      %   ^         m         m    *      %   m     n  m               "<<endl;
+cout<< " #         *      %   $$$$$$$$  m         m    *      %   m      n m                     "<<endl;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}	
