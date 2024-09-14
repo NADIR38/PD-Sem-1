d42baf7ed4203dac89331d275b7084fbc4cec322
NADIR38 <jamalnadir7778@gmail.com>
2024-09-14 01:33:09 -0700

    cow

---
+++ PD 1/cow.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+main()
+{
+cout<< "                   -----------------------------------------                                                                                             "<<endl;
+cout<< "                                                                                                  "<<endl;
+cout<< "                                                                                                  "<<endl;
+cout<< "                            o     ^____^                                                                "<<endl;
+cout<< "                                                                                                  "<<endl;
+cout<< "                              o    (oo)\_____________				                                                               "<<endl;
+cout<< "                                                                                                  "<<endl;
+cout<< "                                   (__)\             )\/\                                                               "<<endl;
+cout<< "                                       || -------w   |                                                         "<<endl;
+cout<< "                                       ||           ||                                                "<<endl;
+cout<< "                                                                                                  "<<endl;
+cout<< "                                                                                                  "<<endl;
+cout<< "                                                                                                  "<<endl;
+cout<< "                                                                                                  "<<endl;
+cout<< "                                                                                                  "<<endl;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
