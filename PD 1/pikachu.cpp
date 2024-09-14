8700dedd27f838f856639411e73bbfb6bea1b311
NADIR38 <jamalnadir7778@gmail.com>
2024-09-14 02:16:59 -0700

    pikachu

---
+++ PD 1/pikachu.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+main()
+{
+cout << "             '._           ---,                                                         "<<endl;
+cout << "             '   .        ' .--'                                                          "<<endl;
+cout << "              '. '!_...._/'                                                                   "<<endl;
+cout << "                 !        /        ,                                                  "<<endl;
+cout << "                 / ()  () !       ,  '-._                                                  "<<endl;
+cout << "                |)   .   ()!     .   _.'                                                    "<<endl;
+cout << "                 |   -'-    |   /    i                                                     "<<endl;
+cout << "                 . .         , i '. <                                                       "<<endl;
+cout << "                 '  --      i|    > |                                                       "<<endl;
+cout << "                / ,      / ',| .-'.-'                                                     "<<endl;
+cout << "               (_/      (_ ,i| .<'                                                              "<<endl;
+cout << "                 !     ,     i-'                                                         "<<endl;
+cout << "                  >    !    /                                                           "<<endl;
+cout << "                  (_,-''> . '                                                                  "<<endl;
+cout << "                    (_,'                                                                 "<<endl;
+cout << "                                                                                     "<<endl;
+
+
+
+}
