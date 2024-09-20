03551c7de840d0bb8521b463f8c3041422f17a95
NADIR38 <jamalnadir7778@gmail.com>
2024-09-19 22:56:39 -0700

    complex

--- pd 2/complex.cpp
+++ pd 2/complex.cpp
@@ -2,38 +2,60 @@
 using namespace std;
 main()
 {
-cout<<"vegetable price per kg..";
-float priceveg;
-cin>>priceveg;
-cout<<"fruit price per kg..";
-float pricefruit;
-cin>>pricefruit;
-cout<<"fruits sold";
-float totalfruit;
-cin>>totalfruit;
-cout<<"total veg sold..";
-float vegkg;
-cin>>vegkg;
-float net;
-net=priceveg*vegkg+pricefruit*totalfruit;
-cout<<"net earning is.."<<net
-
-
-
-
-
-
-
-
-
-
-
-
-
-;
-
-
-
+cout<<"no 1..";
+int one;
+cin>>one;
+cout<<"no 2..";
+int two;
+cin>>two;
+cout<<"no 3..";
+int three;
+cin>>three;
+cout<<"no 4..";
+int four;
+cin>>four;
+cout<<"no 5..";
+int five;
+cin>>five;
+int add;
+add=one+two+three+four+five;
+cout<<"no 6..";
+int six;
+cin>>six;
+cout<<"no 7..";
+int seven;
+cin>>seven;
+cout<<"no 8..";
+int eight;
+cin>>eight;
+cout<<"no 9..";
+int nine;
+cin>>nine;
+cout<<"no 10..";
+int ten;
+cin>>ten;
+int multiply;
+multiply=six*seven*eight*nine*ten;
+cout<<"no 11..";
+int eleven;
+cin>>eleven;
+cout<<"no 12..";
+int twelve;
+cin>>twelve;
+cout<<"no 13..";
+int thirteen;
+cin>>thirteen;
+cout<<"no 14..";
+int fourteen;
+cin>>fourteen;
+cout<<"no 15..";
+int fifteen;
+cin>>fifteen;
+int substract;
+substract=eleven-twelve-thirteen-fourteen-fifteen;
+int result;
+result=(add+multiply)-substract;
+cout<<"result is.."<<result;
 
 
 
