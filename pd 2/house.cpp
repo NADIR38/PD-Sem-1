4d047fb1e8cc476d67bb9d455f19332c583da24e
NADIR38 <jamalnadir7778@gmail.com>
2024-09-19 22:23:34 -0700

    paint

--- pd 2/house.cpp
+++ pd 2/house.cpp
@@ -2,14 +2,15 @@
 using namespace std;
 main()
 {
-cout<<"no 1";
-int one;
-cin>>one;
-cout<<"no 2";
-int two;
-cin>>two;
-
-
+cout<<"person age";
+int age;
+cin>>age;
+cout<<" no of houses moved";
+int house;
+cin>>house;
+int average;
+average=age/(house+1);
+cout<<"average span"<<average;
 
 
 
---
+++ pd 2/paint.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+main()
+{
+cout<<"no of sq unit you can paint..";
+int squnit;
+cin>>squnit;
+cout<<"width of wall..";
+int width;
+cin>>width;
+cout<<"lenght of wall..";
+int lenght;
+cin>>lenght;
+int area;
+area=lenght*width;
+int walls;
+walls=squnit/area;
+cout<<"no of walls you can paint.."<<walls;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
