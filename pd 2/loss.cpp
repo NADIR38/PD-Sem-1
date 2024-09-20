40f5a0802ffaca4299b5dd5392cb3b65ae72c14d
NADIR38 <jamalnadir7778@gmail.com>
2024-09-19 22:35:36 -0700

    loss

---
+++ pd 2/loss.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+main()
+{
+cout<<"target weight loss ..";
+int loss;
+cin>>loss;
+cout<<"no of days to lose 1 kg..";
+int kg1;
+cin>>kg1;
+int totaldays;
+totaldays=(loss/1)*kg1;
+cout<<"no of days to lose waight.."<<totaldays;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
