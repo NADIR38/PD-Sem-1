e7f645fcafe5ceba86d86ea7a08a023cac194edd
NADIR38 <jamalnadir7778@gmail.com>
2024-09-19 20:45:43 -0700

    imposter.cpp

---
+++ pd 2/imposter.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+main()
+{
+cout<<"imposter count..";
+float ic;
+cin>>ic;
+cout<<"total players..";
+float tp;
+cin>>tp;
+float imposter;
+imposter=(ic/tp)*100;
+cout<<"chance of being imposter.."<<imposter;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
