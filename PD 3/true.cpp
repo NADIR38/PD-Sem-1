358661b97c8b2b6e7494945220f63dac9c7f6c4e
NADIR38 <jamalnadir7778@gmail.com>
2024-09-26 21:49:33 -0700

    true

---
+++ PD 3/true.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+void equal();
+main()
+{
+equal();
+
+
+
+
+}
+void equal()
+{
+int n1,n2;
+cout<<"enter first no..";
+cin>>n1;
+cout<<"enter second number";
+cin>>n2;
+if(n1==n2)
+{cout<<"true";}
+if(n1!=n2)
+{
+cout<<"false";
+}
+}
