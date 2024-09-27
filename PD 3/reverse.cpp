b9867aa1db6d2fc897ad46b00322d6f3906b020b
NADIR38 <jamalnadir7778@gmail.com>
2024-09-26 21:40:54 -0700

    reverse

---
+++ PD 3/reverse.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+void reverse();
+main()
+{
+reverse();
+
+
+
+
+}
+void reverse()
+{
+string put;
+cin>>put;
+if(put=="true")
+{cout<<"false";}
+if(put=="false")
+{
+cout<<"true";
+}
+}
